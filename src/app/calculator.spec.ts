80f1ad0a151f3f5027027663d8896f23f5ff2253
Armando Rivera <rivera.armando997@gmail.com>
2022-05-03 11:33:52 -0500

    test suites

--- src/app/calculator.spec.ts
+++ src/app/calculator.spec.ts
@@ -1,36 +1,42 @@
 import { Calculator } from './calculator';
 
-fdescribe('calculator', () => {
+describe('calculator', () => {
   const calculator = new Calculator();
 
-  it('should multiply', () => {
-    const expectedValue = 9;
-    const rta = calculator.multiply(3, 3);
-    expect(rta).toBe(expectedValue);
+  describe('multiply test', () => {
+    it('should multiply', () => {
+      const expectedValue = 9;
+      const rta = calculator.multiply(3, 3);
+      expect(rta).toBe(expectedValue);
+    });
   });
 
-  it('should return null value', () => {
-    const rta = calculator.divide(3, 0);
-    expect(rta).toBeNull();
+  describe('divider test', () => {
+    it('should return null value', () => {
+      const rta = calculator.divide(3, 0);
+      expect(rta).toBeNull();
+    });
+
+    it('should divide', () => {
+      const expectedValue = 1;
+      const rta = calculator.divide(3, 3);
+      expect(rta).toBe(expectedValue);
+    });
   });
 
-  it('should divide', () => {
-    const expectedValue = 1;
-    const rta = calculator.divide(3, 3);
-    expect(rta).toBe(expectedValue);
-  });
+  describe('matchers tests', () => {
+    it('test matchers', () => {
+      let name = 'Armando';
+      let name2;
 
-  it('test matchers', () => {
-    let name = 'Armando';
-    let name2;
-
-    expect(name).toBeDefined();
-    expect(name2).toBeUndefined();
-    expect(1 + 3 === 4).toBeTruthy();
-    expect(1 + 1 === 3).toBeFalsy();
-    expect(5).toBeLessThan(10);
-    expect(20).toBeGreaterThan(10);
-    expect('123456').toMatch(/123/);
-    expect(['apples', 'oranges', 'pears']).toContain('oranges');
+      expect(name).toBeDefined();
+      expect(name2).toBeUndefined();
+      expect(1 + 3 === 4).toBeTruthy();
+      expect(1 + 1 === 3).toBeFalsy();
+      expect(5).toBeLessThan(10);
+      expect(20).toBeGreaterThan(10);
+      expect('123456').toMatch(/123/);
+      expect(['apples', 'oranges', 'pears']).toContain('oranges');
+    });
   });
 });
