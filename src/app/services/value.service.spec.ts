56446a200609bac6d4bf380c5e056ff1b1c8fae6
Armando Rivera <rivera.armando997@gmail.com>
2022-05-03 12:41:13 -0500

    testing a simple service: observer, promise get and set

---
+++ src/app/services/value.service.spec.ts
@@ -0,0 +1,62 @@
+import { ValueService } from './value.service';
+
+fdescribe('ValueService', () => {
+  let service: ValueService;
+
+  beforeEach(() => {
+    service = new ValueService();
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+
+  describe('Test for getValue', () => {
+    it('should return "my value" ', () => {
+      const expectedValue = 'my value';
+      const rta = service.getValue();
+      expect(rta).toBe(expectedValue);
+    });
+  });
+
+  describe('Test for setValue', () => {
+    it('should change the value', () => {
+      const expectedValue = 'other value';
+      service.setValue(expectedValue);
+      const rta = service.getValue();
+      expect(rta).toBe(expectedValue);
+    });
+  });
+
+  describe('Test for getPromiseValue', () => {
+    it('should get the promise value using then', (doneFn) => {
+      const expectedValue = 'Promise - value';
+      service.getPromise().then((value) => {
+        expect(value).toBe(expectedValue);
+        doneFn();
+      });
+    });
+
+    it('should get the promise value using async await', async () => {
+      const expectedValue = 'Promise - value';
+      const rta = await service.getPromise();
+      expect(rta).toBe(expectedValue);
+    });
+  });
+
+  describe('Test for getObservableValue using done', () => {
+    it('Should get the observer value', (done) => {
+      const expectedValue = 'Observable - value';
+      service.getObservable().subscribe((value) => {
+        expect(value).toBe(expectedValue);
+        done();
+      });
+    });
+
+    it('Should get the observer value toPromise', async () => {
+      const expectedValue = 'Observable - value';
+      const value = await service.getObservable().toPromise();
+      expect(value).toBe(expectedValue);
+    });
+  });
+});
