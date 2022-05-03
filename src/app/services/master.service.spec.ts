b39a644a80ca4634b387d22aeaaf852b0c60fd9a
Armando Rivera <rivera.armando997@gmail.com>
2022-05-03 13:18:00 -0500

    testign services using spies

--- src/app/services/master.service.spec.ts
+++ src/app/services/master.service.spec.ts
@@ -2,7 +2,7 @@ import { MasterService } from './master.service';
 import { FakeValueService } from './value-fake.service';
 import { ValueService } from './value.service';
 
-describe('MasterService', () => {
+fdescribe('MasterService', () => {
   it('should return "my value" from the real service', () => {
     const valueService = new ValueService();
     const masterService = new MasterService(valueService);
@@ -24,5 +24,15 @@ describe('MasterService', () => {
       fakeValueService as unknown as ValueService
     );
     expect(masterService.getValue()).toBe(value);
+  });
+
+  it('should call getValue from value service', () => {
+    const valueServiceSpy = jasmine.createSpyObj('ValueService', ['getValue']);
+    valueServiceSpy.getValue.and.returnValue('fake value');
+    const masterService = new MasterService(valueServiceSpy);
+
+    expect(masterService.getValue()).toBe('fake value');
+    expect(valueServiceSpy.getValue).toHaveBeenCalled();
+    expect(valueServiceSpy.getValue).toHaveBeenCalledTimes(1);
   });
 });
