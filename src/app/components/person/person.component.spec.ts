ad93560e926af0d6a7fec8492bd5f4d112c1a8a3
Armando Rivera <rivera.armando997@gmail.com>
2022-05-05 14:46:59 -0500

    components: debugElement and query by.css

--- src/app/components/person/person.component.spec.ts
+++ src/app/components/person/person.component.spec.ts
@@ -1,4 +1,6 @@
+import { DebugElement } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 
 import { PersonComponent } from './person.component';
 
@@ -23,9 +25,18 @@ fdescribe('PersonComponent', () => {
   });
 
   it('should have a <p> with "Im a paragraph"', () => {
+    const personDebug: DebugElement = fixture.debugElement;
     const textExpected = 'Im a paragraph';
-    const personElement: HTMLElement = fixture.nativeElement;
+    const personElement: HTMLElement = personDebug.nativeElement;
     const p = personElement.querySelector('p');
     expect(p.textContent).toBe(textExpected);
+  });
+
+  it('should have a <h3> with "Hello, Im a person component"', () => {
+    const personDebug: DebugElement = fixture.debugElement;
+    const h3Debug: DebugElement = personDebug.query(By.css('h3'));
+    const textExpected = 'Hello, Im a person component';
+    const h3Element: HTMLElement = h3Debug.nativeElement;
+    expect(h3Element.textContent).toBe(textExpected);
   });
 });
