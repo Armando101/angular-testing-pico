a313ac77e30c429fa1d27c07ded5d0485ba0bb85
Armando Rivera <rivera.armando997@gmail.com>
2022-05-06 01:02:00 -0500

    components: Testing a component from parent component

--- src/app/components/person-list/person-list.component.ts
+++ src/app/components/person-list/person-list.component.ts
@@ -7,8 +7,18 @@ import { Person } from 'src/app/models/person.model';
   styleUrls: ['./person-list.component.scss'],
 })
 export class PersonListComponent implements OnInit {
-  person: Person = new Person('Armando', 'Rivera', 24, 80, 180);
+  person: Person = new Person('Armando', 'Rivera', 24, 80, 1.8);
+  personList: Person[] = [
+    new Person('Peter', 'Parker', 24, 70, 1.7),
+    new Person('Armando', 'Rivera', 24, 80, 1.8),
+  ];
+  selectedPerson: Person;
+
   constructor() {}
 
   ngOnInit(): void {}
+
+  choose(person: Person) {
+    this.selectedPerson = person;
+  }
 }
