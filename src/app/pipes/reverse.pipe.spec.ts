590c9fe5e20b32f698993ed7c35eae315abe7d09
Armando Rivera <rivera.armando997@gmail.com>
2022-05-10 14:37:50 -0500

    pipes usint host component

--- src/app/pipes/reverse.pipe.spec.ts
+++ src/app/pipes/reverse.pipe.spec.ts
@@ -1,3 +1,7 @@
+import { Component } from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
+import { By } from '@angular/platform-browser';
 import { ReversePipe } from './reverse.pipe';
 
 fdescribe('ReversePipe', () => {
@@ -10,5 +14,53 @@ fdescribe('ReversePipe', () => {
     const pipe = new ReversePipe();
     const rta = pipe.transform('roma');
     expect(rta).toBe('amor');
+  });
+});
+
+@Component({
+  template: `
+    <h5>{{ 'amor' | reverse }}</h5>
+    <input [(ngModel)]="text" />
+    <p>{{ text | reverse }}</p>
+  `,
+})
+class Hostcomponent {
+  text = '';
+}
+
+fdescribe('ReservePipe from Hostcomponent', () => {
+  let component: Hostcomponent;
+  let fixture: ComponentFixture<Hostcomponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [Hostcomponent, ReversePipe],
+      imports: [FormsModule],
+    }).compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(Hostcomponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should h5 be "roma"', () => {
+    const debugh5 = fixture.debugElement.query(By.css('h5'));
+    expect(debugh5.nativeElement.textContent).toBe('roma');
+  });
+
+  it('should apply reverse pipe when typing in the input', () => {
+    const inputdebug = fixture.debugElement.query(By.css('input'));
+    const inputElement: HTMLInputElement = inputdebug.nativeElement;
+    const pdebug = fixture.debugElement.query(By.css('p'));
+
+    expect(pdebug.nativeElement.textContent).toBeFalsy();
+
+    inputElement.value = 'my text';
+    inputElement.dispatchEvent(new Event('input'));
+    fixture.detectChanges();
+
+    expect(pdebug.nativeElement.textContent).toBe('txet ym');
   });
 });
