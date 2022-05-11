3605eaf1603c5e16c72d046b9bf3687794192605
Armando Rivera <rivera.armando997@gmail.com>
2022-05-11 12:32:57 -0500

    configure setup for testing forms

---
+++ src/app/auth/components/register-form/register-form.component.spec.ts
@@ -0,0 +1,30 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
+import { UsersService } from 'src/app/services/user.service';
+
+import { RegisterFormComponent } from './register-form.component';
+
+fdescribe('RegisterFormComponent', () => {
+  let component: RegisterFormComponent;
+  let fixture: ComponentFixture<RegisterFormComponent>;
+  let userService: jasmine.SpyObj<UsersService>;
+
+  beforeEach(async () => {
+    const userServiceSpy = jasmine.createSpyObj('UsersService', ['create']);
+    await TestBed.configureTestingModule({
+      declarations: [RegisterFormComponent],
+      imports: [ReactiveFormsModule],
+      providers: [{ provide: UsersService, useValue: userServiceSpy }],
+    }).compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(RegisterFormComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
