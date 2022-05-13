3c0addc19c041982662a6718054e90232dfc1048
Armando Rivera <rivera.armando997@gmail.com>
2022-05-13 13:40:30 -0500

    testing render router params

--- src/app/products/components/product-detail/product-detail.component.spec.ts
+++ src/app/products/components/product-detail/product-detail.component.spec.ts
@@ -2,7 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ActivatedRoute } from '@angular/router';
 import { generateOneProduct } from 'src/app/mocks/product.mock';
 import { ProductsService } from 'src/app/services/product.service';
-import { ActivatedRouteStub, mockObservable } from 'src/testing';
+import { ActivatedRouteStub, getText, mockObservable } from 'src/testing';
 
 import { ProductDetailComponent } from './product-detail.component';
 
@@ -47,7 +47,9 @@ fdescribe('ProductDetailComponent', () => {
       ProductsService
     ) as jasmine.SpyObj<ProductsService>;
     location = TestBed.inject(Location) as jasmine.SpyObj<Location>;
+  });
 
+  it('should create', () => {
     const productId = '1';
     route.setParamMap({ id: productId });
 
@@ -59,9 +61,26 @@ fdescribe('ProductDetailComponent', () => {
     productsService.getOne.and.returnValue(mockObservable(productMock));
 
     fixture.detectChanges();
+    expect(component).toBeTruthy();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should show the product in the view', () => {
+    const productId = '2';
+    route.setParamMap({ id: productId });
+
+    const productMock = {
+      ...generateOneProduct(),
+      id: productId,
+    };
+
+    productsService.getOne.and.returnValue(mockObservable(productMock));
+
+    fixture.detectChanges();
+    const titleText = getText(fixture, 'title');
+    const priceText = getText(fixture, 'price');
+
+    expect(titleText).toContain(productMock.title);
+    expect(priceText).toContain(`${productMock.price}`);
+    expect(productsService.getOne).toHaveBeenCalledWith(productId);
   });
 });
